0ac9389bdd7077c0758e565bd806c2d18c7310cb
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-05-21 17:53:10 +0800

    Update coco_annotation_comment.py

--- commented/coco_annotation_comment.py
+++ commented/coco_annotation_comment.py
@@ -4,16 +4,16 @@ from collections import defaultdict
 name_box_id = defaultdict(list)
 id_name = dict()
 f = open(
-    "mscoco2017/annotations/instances_train2017.json",#讀取.json檔
+    "mscoco2017/annotations/instances_train2017.json",  # 讀取.json檔
     encoding='utf-8')
 data = json.load(f)
 
 annotations = data['annotations']
 for ant in annotations:
     id = ant['image_id']
-    name = 'mscoco2017/train2017/%012d.jpg' % id#載入圖片
+    name = 'mscoco2017/train2017/%012d.jpg' % id  # 載入圖片
     cat = ant['category_id']
-    #種類的分類
+    # 種類的分類
     if cat >= 1 and cat <= 11:
         cat = cat - 1
     elif cat >= 13 and cat <= 25:
@@ -33,20 +33,22 @@ for ant in annotations:
     elif cat >= 84 and cat <= 90:
         cat = cat - 11
 
-    name_box_id[name].append([ant['bbox'], cat])#輸出boundingbox種類
+    name_box_id[name].append([ant['bbox'], cat]) # 輸出boundingbox種類
 
-f = open('train.txt', 'w')#讀取train.txt檔
+f = open('train.txt', 'w') # 讀取train.txt檔
 for key in name_box_id.keys():
     f.write(key)
     box_infos = name_box_id[key]
     for info in box_infos:
-        x_min = int(info[0][0])#boundingbox左上角x座標
-        y_min = int(info[0][1])#boundingbox左上角y座標
-        x_max = x_min + int(info[0][2])#boundingbox右下角x座標
-        y_max = y_min + int(info[0][3])#boundingbox右下角y座標
+        x_min = int(info[0][0]) # boundingbox左上角x座標
+        y_min = int(info[0][1]) # boundingbox左上角y座標
+        x_max = x_min + int(info[0][2]) # boundingbox右下角x座標
+        y_max = y_min + int(info[0][3]) # boundingbox右下角y座標
 
         box_info = " %d,%d,%d,%d,%d" % (
             x_min, y_min, x_max, y_max, int(info[1]))#boundingbox座標
         f.write(box_info)#寫入train.txt中
     f.write('\n')
 f.close()
+
+
